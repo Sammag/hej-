5a7d234175b71c7b7deb7ae19812136e0c536350
Samuel Magnusson <masa16@nti.umea>
2018-11-21 11:11:23 +0100

    hej

--- monika.py
+++ monika.py
@@ -1,12 +1,12 @@
 # Program för att hitta summan av alla nummer som är lagrade i en lista
 
 # Lista på nummer som man kan ändra 
-numbers = [6, 5, 3, 8, 3, 2, 5, 4, 11,#]
+numbers = [6, 5, 3, 8, 3, 2, 5, 4, 11]
 
 # variabel för att lagra summan
 summa = 0
 
-# iterera över listan
+# iterera över listan.
 for val in numbers:
 	summa = summa+val
 
