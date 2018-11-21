3b61d7b55b5aa04f86132dd572e42b209edd3cf1
Samuel Magnusson <masa16@nti.umea>
2018-11-21 11:09:15 +0100

    bru

--- monika.py
+++ monika.py
@@ -1,7 +1,14 @@
-hej = "monika" 
-hej = 1
-hej = ord("a")
-hej = chr(10)
+# Program för att hitta summan av alla nummer som är lagrade i en lista
 
-print("värde")
-print("hej")
+# Lista på nummer som man kan ändra 
+numbers = [6, 5, 3, 8, 3, 2, 5, 4, 11,#]
+
+# variabel för att lagra summan
+summa = 0
+
+# iterera över listan
+for val in numbers:
+	summa = summa+val
+
+# summan 57
+print("The sum is", summa)
